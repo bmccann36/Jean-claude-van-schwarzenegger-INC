6a0df4c65cb6dcb7ec165c34b2c34bbe2a751497
Brian McCann <Brian@Users-MBP.home>
2017-11-06 22:56:37 -0500

    got the price fix hook working

--- server/db/models/order.js
+++ server/db/models/order.js
@@ -6,30 +6,31 @@ const OrderProduct = require('./orderProduct')
 
 // userId should be inserted as a foreign key
 const Order = db.define('order', {
-  status:{
+  status: {
     type: Sequelize.ENUM,
     values: ['pending', 'ordered', 'shipped', 'delivered'] //maybe we could make a defualt pending value for
-    }
+  }
 
 })
 
 //after update of values change price in orderProduct
 Order.hook('afterUpdate', order => {
-  if(order.status == 'ordered'){
+  if (order.status == 'ordered') {
 
-    OrderProduct.findOne({where: {orderId: 1}})
-    .then(orderedProduct => {
-      magenta('ordered product')
-      console.log(orderedProduct.dataValues)
-      return orderedProduct.update({price: 10})
-    })
+    OrderProduct.findAll({ where: { orderId: order.id } }) // hard coded needs fixing
+      .then(orderedProducts => {
+        const updatePromise = orderedProducts.map(orderItem => {
+          orderItem.update({ price: 10 })
+        })
+        Promise.all(updatePromise)
+      })
   }
 })
 
 // magenta('hello world')
 
-function magenta(str){
-  console.log (chalk.magenta(str))
+function magenta(str) {
+  console.log(chalk.magenta(str))
 }
 
 
