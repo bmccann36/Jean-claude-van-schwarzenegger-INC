f25c86df5c4d45a78c5e3a240056768104ab6c51
Brian McCann <bmccann36@gmail.com>
2017-11-07 17:45:23 -0500

    created an algo to get order details on order items

--- client/components/Cart.js
+++ client/components/Cart.js
@@ -1,61 +1,53 @@
 import React, { Component } from 'react'
-import {NavLink, withRouter} from 'react-router-dom'
+import { NavLink, withRouter } from 'react-router-dom'
+import store from '../store'
 import { connect } from 'react-redux'
-import axios from 'axios'
-import { incrementInDb, addProductToDb, changeStatusDb } from '../store/order'
-import user from "../store/user";
+
+import { incrementInDb, addProductToDb } from '../store/order'
+
 
 
 class Cart extends Component {
-
-    constructor(props){
-      super(props)
-
-      this.state = {
-           details : {}
-      }
-    }
-
-  componentDidMount() {
-
-      let userId = this.props.user.id
-
-    axios.get(`/api/orders/detail/${userId}`)
-      .then(res=> res.data)
-      .then(details => this.setState(details))
+  constructor(props) {
+    super(props)
 
   }
 
 
-   render(){
-     const  detail  = this.state
-     //let details = this.state.details.length && this.state['0'].products
-     console.log('details', detail);
+  render() {
+    let products
+    let orderItems
+    if (this.props) {
+      products = this.props.product
+      orderItems = this.props.order
+    }
+  orderItems.forEach( orderItem => {
+    const getDetail = findProduct(products, orderItem.productId)
+    orderItem.details = getDetail[0]
+  })
+  console.log(orderItems, 'orderItems')
 
-     let orders = this.props.order;
-     let products = this.props.product;
+    return (
 
-   return (
-
-    <div>
-       {/*<ul>*/}
-      {/*{*/}
+      <div>
+        {/* <ul> */}
+        {/*{*/}
         {/*orders.length && orders.map(order=> {*/}
-          {/*return (*/}
-            {/*<h1 key={order.productId}>{order.quantity}</h1>*/}
-          {/*)*/}
+        {/*return (*/}
+        {/*<h1 key={order.productId}>{order.quantity}</h1>*/}
+        {/*)*/}
         {/*})*/}
-      {/*}*/}
-    {/*</ul>*/}
+        {/*}*/}
+        {/*</ul>*/}
 
-      <NavLink to="/checkout">
+        {/* <NavLink to="/checkout">
         <button className="sub-btn"><small>Checkout</small></button>
-      </NavLink>
+      </NavLink> */}
+        <h1> HELLO </h1>
 
-
-    </div>
-  )
-   }
+      </div>
+    )
+  }
 
 
 }
@@ -70,7 +62,11 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = { incrementInDb, addProductToDb }
 
-// call connect function from react-redux, pass it mapState, and invoke with the presentational component (this component itself)
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Cart))
-// don't need map dispatch yet because no methods being called
 
+
+
+
+function findProduct(products, productId) {
+  return products.filter(product => productId === product.id)
+}
